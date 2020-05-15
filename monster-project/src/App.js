122de2f597bfd23c61e92f10cb554b9aa324ab2f
Tanner Langlinais <tlanglinais@gmail.com>
2020-05-14 20:44:27 -0500

    Lesson 32

--- monster-project/src/App.js
+++ monster-project/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
+import CardList from "./components/card-list/card-list.component";
+
 import "./App.css";
 
 class App extends Component {
@@ -7,29 +9,20 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstein",
-          id: "asc1",
-        },
-        {
-          name: "Dracula",
-          id: "asc2",
-        },
-        {
-          name: "Zombie",
-          id: "asc3",
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters}></CardList>
       </div>
     );
   }
