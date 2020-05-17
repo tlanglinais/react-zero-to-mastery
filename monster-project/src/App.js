09230a879555b28e946bddd2e56282ebf83f6e1c
Tanner Langlinais <tlanglinais@gmail.com>
2020-05-17 09:55:15 -0500

    starting clothing app

--- monster-project/src/App.js
+++ monster-project/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import CardList from "./components/card-list/card-list.component";
-
+import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
 class App extends Component {
@@ -10,6 +10,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -19,11 +20,20 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <input type="search" placeholder="search monsters" />
-        <CardList monsters={this.state.monsters}></CardList>
+        <h1> Monsters Rolodex </h1>
+        <SearchBox placeholder="search monsters" onChange={this.handleChange} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
---
+++ monster-project/src/components/search-box/search-box.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./search-box.styles.css";
+
+const SearchBox = (props) => {
+  return (
+    <input
+      className="search"
+      type="search"
+      placeholder={props.placeholder}
+      onChange={props.handleChange}
+    />
+  );
+};
+
+export default SearchBox;
