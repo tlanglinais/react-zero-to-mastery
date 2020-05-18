4fc62acd799c941f51451f9a258ec1c44d9f4f62
Tanner Langlinais <tlanglinais@gmail.com>
2020-05-17 19:05:23 -0500

    Starting forms

--- clothing-app/src/App.js
+++ clothing-app/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage.component";
 
+const HatsPage = () => {
+  <div>HATS PAGE</div>;
+};
+
 function App() {
   return (
     <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
       <HomePage />
     </div>
   );
