7545caeebedf8dc1973f464ce13fb3cca33876dc
Tanner Langlinais <tlanglinais@gmail.com>
2020-05-22 15:00:31 -0500

    Starting redux

--- clothing-app/src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ clothing-app/src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import SignIn from "../sign-in/sign-in.component";
+import SignUp from "../sign-up/sign-up.component";
 
 import "./sign-in-and-sign-up.styles.scss";
 
@@ -8,6 +9,7 @@ export const SignInAndSignUpPage = () => {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
