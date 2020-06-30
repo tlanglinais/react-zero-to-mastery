f758d77acaafaaa23da31eaf21705cb5227b6d3a
Tanner Langlinais <tlanglinais@gmail.com>
2020-06-30 12:12:56 -0500

    Adding folder for hoc example

--- clothing-app/src/redux/shop/shop.selectors.js
+++ clothing-app/src/redux/shop/shop.selectors.js
@@ -8,12 +8,12 @@ export const selectCollections = createSelector(
 );
 
 export const selectCollectionsForPreview = createSelector(
-  [selectShop],
-  (collections) => Object.keys(collections).map((_key) => collections[_key])
+  [selectCollections],
+  (collections) =>
+    collections ? Object.keys(collections).map((_key) => collections[_key]) : []
 );
 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector(
-    [selectCollections],
-    (collections) => collections[collectionUrlParam]
+  createSelector([selectCollections], (collections) =>
+    collections ? collections[collectionUrlParam] : null
   );
