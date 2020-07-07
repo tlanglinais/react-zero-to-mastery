bd9f44515ace50db627ad74851b31357575dfdbb
Tanner Langlinais <tlanglinais@gmail.com>
2020-07-07 09:37:18 -0500

    Added backend for stripe integration

--- clothing-app/client/src/components/stripe-button/stripe-button.component.jsx
+++ clothing-app/client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51GqjtnElUvemfCnbLxOhkQzrtDMMIwHQk2nOo8yKdTuzCEXr5cbVkQ16PeJl2eGBGDOPgNWm4bih9lmiOeoH6ddN00ucaEZ7HR";
 
-  const onToken = (token) => {
-    console.log(token);
-    alert("Payment successful");
+  const onToken = token => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amound: priceForStripe,
+        token,
+      },
+    })
+      .then(response => {
+        alert("Payment successful");
+      })
+      .catch(error => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert("There was an issue with your payment.");
+      });
   };
 
   return (
